e85eb20a470a2f102e244696cd0fc16c31ca794d
benimhoff <simplementebennett@gmail.com>
2025-05-30 14:47:45 -0300

    First commit

---
+++ src/main/java/com/puntografico/puntografico/PuntograficoApplication.java
@@ -0,0 +1,13 @@
+package com.puntografico.puntografico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuntograficoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuntograficoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/puntografico/puntografico/PuntograficoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puntografico.puntografico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuntograficoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
