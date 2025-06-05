e29a879e8a5198695ba8d7ad122ebcebf51f6112
benimhoff <simplementebennett@gmail.com>
2025-06-04 22:17:43 -0300

    Agregamos CarpetaConSolapa

--- doc/schema.sql
+++ doc/schema.sql
@@ -323,6 +323,18 @@ create table if not exists cuaderno_anillado (
 	constraint fk_cuaderno_anillado_orden foreign key (id_orden_trabajo) references orden_trabajo(id)
 );
 
+create table if not exists carpeta_con_solapa (
+	id bigint auto_increment not null primary key,
+	tipo_laminado varchar(255) not null,
+	tipo_papel_hojas varchar(255) not null,
+	tipo_faz varchar(255) not null,
+	adicional_disenio tinyint(1) null,
+	archivo varchar(255) null,
+	detalle varchar(255) null,
+	id_orden_trabajo bigint not null unique,
+	constraint fk_carpeta_con_solapa_orden foreign key (id_orden_trabajo) references orden_trabajo(id)
+);
+
 -- insertar datos a las tablas iniciales
 insert into rol(nombre) values
 ('ADMIN'),
@@ -360,6 +372,7 @@ insert into categoria_producto(nombre) values
 ('CIERRA_BOLSAS'),
 ('TURNERO'),
 ('OTROS')
-('CUADERNO_ANILLADO');
+('CUADERNO_ANILLADO'),
+('CARPETA_CON_SOLAPA');
 
 
