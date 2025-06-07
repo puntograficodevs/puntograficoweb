eecc2f2de879c9ab39f035d8f7163fe2b250dbd5
benimhoff <simplementebennett@gmail.com>
2025-06-07 19:40:50 -0300

    Armando nueva config. para tener precios estáticos

--- src/main/java/com/puntografico/puntografico/repository/ItemPersonalizableRepository.java
+++
@@ -1,9 +0,0 @@
-package com.puntografico.puntografico.repository;
-
-import com.puntografico.puntografico.domain.ItemPersonalizable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemPersonalizableRepository extends JpaRepository<ItemPersonalizable, Long> {
-}
