365a3605be06bd3cdd00bff36f5433d84481c516
benimhoff <imhoff.bennett@gmail.com>
2025-08-13 15:21:32 -0300

    Impresion - todos los controller hechos
    Seguir con:
    Frontend

---
+++ src/main/java/com/puntografico/puntografico/controller/ImpresionController.java
@@ -0,0 +1,72 @@
+package com.puntografico.puntografico.controller;
+
+import com.puntografico.puntografico.domain.*;
+import com.puntografico.puntografico.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+public class ImpresionController {
+
+    @Autowired
+    private OpcionesImpresionService opcionesImpresionService;
+
+    @Autowired
+    private MedioPagoService medioPagoService;
+
+    @Autowired
+    private OrdenTrabajoService ordenTrabajoService;
+
+    @Autowired
+    private ImpresionService impresionService;
+
+    @Autowired
+    private OrdenImpresionService ordenImpresionService;
+
+    @GetMapping("/crear-odt-impresion")
+    public String verCrearOdtImpresion(Model model) {
+        List<TipoColorImpresion> listaTipoColorImpresion = opcionesImpresionService.buscarTodosTipoColorImpresion();
+        List<TamanioHojaImpresion> listaTamanioHojaImpresion = opcionesImpresionService.buscarTodosTamanioHojaImpresion();
+        List<TipoFazImpresion> listaTipoFazImpresion = opcionesImpresionService.buscarTodosTipoFazImpresion();
+        List<TipoPapelImpresion> listaTipoPapelImpresion = opcionesImpresionService.buscarTodosTipoPapelImpresion();
+        List<CantidadImpresion> listaCantidadImpresion = opcionesImpresionService.buscarTodosCantidadImpresion();
+        List<TipoImpresion> listaTipoImpresion = opcionesImpresionService.buscarTodosTipoImpresion();
+        List<MedioPago> listaMediosDePago = medioPagoService.buscarTodos();
+
+        model.addAttribute("impresion", new Impresion());
+        model.addAttribute("listaTipoColorImpresion", listaTipoColorImpresion);
+        model.addAttribute("listaTamanioHojaImpresion", listaTamanioHojaImpresion);
+        model.addAttribute("listaTipoFazImpresion", listaTipoFazImpresion);
+        model.addAttribute("listaTipoPapelImpresion", listaTipoPapelImpresion);
+        model.addAttribute("listaCantidadImpresion", listaCantidadImpresion);
+        model.addAttribute("listaTipoImpresion", listaTipoImpresion);
+        model.addAttribute("listaMediosDePago", listaMediosDePago);
+
+        return "crear-odt-impresion";
+    }
+
+    @GetMapping("/mostrar-odt-impresion/{ordenImpresionId}")
+    public String verOrdenImpresion(@PathVariable("ordenImpresionId") Long ordenImpresionId, Model model) {
+        OrdenImpresion ordenImpresion = ordenImpresionService.buscarPorId(ordenImpresionId);
+
+        model.addAttribute("ordenImpresion", ordenImpresion);
+
+        return "mostrar-odt-impresion";
+    }
+
+    @PostMapping("/api/creacion-impresion")
+    public String creacionImpresion(HttpServletRequest request) {
+        OrdenTrabajo ordenTrabajo = ordenTrabajoService.crear(request);
+        Impresion impresion = impresionService.crear(request);
+        OrdenImpresion ordenImpresion = ordenImpresionService.crear(ordenTrabajo, impresion);
+
+        return "redirect:/mostrar-odt-impresion/" + ordenImpresion.getId();
+    }
+}
---
+++ src/main/java/com/puntografico/puntografico/controller/PlantillaImpresionRestController.java
@@ -0,0 +1,40 @@
+package com.puntografico.puntografico.controller;
+
+import com.puntografico.puntografico.domain.PlantillaImpresion;
+import com.puntografico.puntografico.repository.PlantillaImpresionRepository;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/plantilla-impresion")
+public class PlantillaImpresionRestController {
+
+    @Autowired
+    private PlantillaImpresionRepository plantillaImpresionRepository;
+
+    @GetMapping("/precio")
+    public ResponseEntity<Integer> obtenerPrecio(
+            @RequestParam Long tipoColorImpresionId,
+            @RequestParam Long tamanioHojaImpresionId,
+            @RequestParam Long tipoFazImpresionId,
+            @RequestParam Long tipoPapelImpresionId,
+            @RequestParam Long cantidadImpresionId,
+            @RequestParam Long tipoImpresionId
+    ) {
+        Optional<PlantillaImpresion> plantilla = plantillaImpresionRepository
+                .findByTipoColorImpresion_IdAndTamanioHojaImpresion_IdAndTipoFazImpresion_IdAndTipoPapelImpresion_IdCantidadImpresion_IdAndTipoImpresion_Id(
+                        tipoColorImpresionId, tamanioHojaImpresionId, tipoFazImpresionId, tipoPapelImpresionId, cantidadImpresionId, tipoImpresionId
+                );
+
+        return plantilla
+                .map(p -> ResponseEntity.ok(p.getPrecio()))
+                .orElse(ResponseEntity.noContent().build());
+    }
+}
