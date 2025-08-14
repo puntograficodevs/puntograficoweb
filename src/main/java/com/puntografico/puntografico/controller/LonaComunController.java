150bdd52712ae179b63a108e8c46313d579ff1a1
benimhoff <imhoff.bennett@gmail.com>
2025-08-14 18:35:14 -0300

    Lona común - Controladores

---
+++ src/main/java/com/puntografico/puntografico/controller/LonaComunController.java
@@ -0,0 +1,64 @@
+package com.puntografico.puntografico.controller;
+
+import com.puntografico.puntografico.domain.*;
+import com.puntografico.puntografico.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+public class LonaComunController {
+
+    @Autowired
+    private OpcionesLonaComunService opcionesLonaComunService;
+
+    @Autowired
+    private MedioPagoService medioPagoService;
+
+    @Autowired
+    private OrdenTrabajoService ordenTrabajoService;
+
+    @Autowired
+    private LonaComunService lonaComunService;
+
+    @Autowired
+    private OrdenLonaComunService ordenLonaComunService;
+
+    @GetMapping("/crear-odt-lona-comun")
+    public String verCrearOdtLonaComun(Model model) {
+        List<MedidaLonaComun> listaMedidaLonaComun = opcionesLonaComunService.buscarTodosMedidaLonaComun();
+        List<TipoLonaComun> listaTipoLonaComun = opcionesLonaComunService.buscarTodosTipoLonaComun();
+        List<MedioPago> listaMediosDePago = medioPagoService.buscarTodos();
+
+        model.addAttribute("lonaComun", new LonaComun());
+        model.addAttribute("listaMedidaLonaComun", listaMedidaLonaComun);
+        model.addAttribute("listaTipoLonaComun", listaTipoLonaComun);
+        model.addAttribute("listaMediosDePago", listaMediosDePago);
+
+        return "crear-odt-lona-comun";
+    }
+
+    @GetMapping("/mostrar-odt-lona-comun/{ordenLonaComunId}")
+    public String verOrdenLonaComun(@PathVariable("ordenLonaComunId") Long ordenLonaComunId, Model model) {
+        OrdenLonaComun ordenLonaComun = ordenLonaComunService.buscarPorId(ordenLonaComunId);
+
+        model.addAttribute("ordenLonaComun", ordenLonaComun);
+
+        return "mostrar-odt-lona-comun";
+    }
+
+    @PostMapping("/api/creacion-lona-comun")
+    public String creacionProducto(HttpServletRequest request) {
+        OrdenTrabajo ordenTrabajo = ordenTrabajoService.crear(request);
+        LonaComun lonaComun = lonaComunService.crear(request);
+        OrdenLonaComun ordenLonaComun = ordenLonaComunService.crear(ordenTrabajo, lonaComun);
+
+        return "redirect:/mostrar-odt-lona-comun/" + ordenLonaComun.getId();
+    }
+}
---
+++ src/main/java/com/puntografico/puntografico/controller/PlantillaLonaComunRestController.java
@@ -0,0 +1,36 @@
+package com.puntografico.puntografico.controller;
+
+import com.puntografico.puntografico.domain.PlantillaLonaComun;
+import com.puntografico.puntografico.repository.PlantillaLonaComunRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/plantilla-lona-comun")
+public class PlantillaLonaComunRestController {
+
+    @Autowired
+    private PlantillaLonaComunRepository plantillaLonaComunRepository;
+
+    @GetMapping("/precio")
+    public ResponseEntity<Integer> obtenerPrecio(
+            @RequestParam boolean conBolsillos,
+            @RequestParam Long medidaLonaComunId,
+            @RequestParam Long tipoLonaComunId
+    ) {
+        Optional<PlantillaLonaComun> plantilla = plantillaLonaComunRepository.
+                findByConBolsillosAndMedidaLonaComun_IdAndTipoLonaComun_Id(
+                        conBolsillos, medidaLonaComunId, tipoLonaComunId
+                );
+
+        return plantilla
+                .map(p -> ResponseEntity.ok(p.getPrecio()))
+                .orElse(ResponseEntity.noContent().build());
+    }
+}
