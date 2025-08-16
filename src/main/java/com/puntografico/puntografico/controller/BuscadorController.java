e7a7c452b539745c5a704584a710b524efe600e4
benimhoff <imhoff.bennett@gmail.com>
2025-08-16 19:59:35 -0300

    navbar con estética ux mari

---
+++ src/main/java/com/puntografico/puntografico/controller/BuscadorController.java
@@ -0,0 +1,29 @@
+package com.puntografico.puntografico.controller;
+
+import com.puntografico.puntografico.domain.Empleado;
+import com.puntografico.puntografico.service.EmpleadoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class BuscadorController {
+
+    @Autowired
+    private EmpleadoService empleadoService;
+
+    @GetMapping("/buscador")
+    public String buscador(HttpSession session, Model model) {
+        Empleado empleado = (Empleado) session.getAttribute("empleadoLogueado");
+
+        if (empleado == null) {
+            return "redirect:/"; // Si no hay sesión, lo manda al login
+        }
+
+        model.addAttribute("empleado", empleado);
+        return "buscador";
+    }
+}
